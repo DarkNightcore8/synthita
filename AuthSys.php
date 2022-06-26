1e2313f1b78c21da8ffd3878e3ab884db1dd4ce7
DarkNightcore <DarkNightcore8@outlook.com>
2022-06-26 23:26:23 +0200

    website files

---
+++ AuthSys.php
@@ -0,0 +1,18 @@
+<?php 
+class AuthSys {
+    private $PD0;
+     
+     public function __construct($PD0conn) {
+         $thisPD0 = $PD0conn;
+     }
+
+     public function registraNuovoUtente() {}
+
+     public function login() {}
+
+     public function logout() {}
+
+     public function utenteLoggati() {}
+
+}
+<?
---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+   define('DB_SERVER', 'sql212.byetcluster.com');
+   define('DB_USERNAME', 'epiz_29224410');
+   define('DB_PASSWORD', '0IvU9nmb2eB');
+   define('DB_DATABASE', 'epiz_29224410_login');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
